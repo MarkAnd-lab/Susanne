5ff57ffc327bdaaf1038a7ccbafed15cbdd71533
MarkAnd-lab <mark-andersson@hotmail.com>
2020-08-20 10:19:37 +0200

    Products

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Products from './components/Products';
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { products:[], filteredProducts: []};
+  }
+  componentWillMount(){
+    fetch("http://localhost:3000/products").then(res => res.json())
+    .then(data => this.setState({
+      products: data,
+      filteredProducts: data
 
-function App() {
+    }))
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+<h1>Susanne Shoping Cart</h1>
+<hr/>
+<div className="row">
+  <div className="col-md-8">
+
+  </div>
+  <div className="col-md-4">
+<Products products={this.state.filteredProducts} handledAddToCart={this.handledAddToCart}/>
+  </div>
+</div>
+      
     </div>
   );
+}
 }
 
 export default App;
