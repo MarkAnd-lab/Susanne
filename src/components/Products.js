27f82454275bb67ba93adb060d3e842f8dd17fed
MarkAnd-lab <mark-andersson@hotmail.com>
2020-08-25 14:24:15 +0200

    txt

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,7 +6,7 @@ export default class Products extends Component {
             <div className="col-md-4">
             <div className="thumbnail text-center">
             <a href={`/${product.id}`} onClick={this.props.handleAddToCard}>
-                    <img src={`/products/${product.sku}_1.jpg`} alt={product.title} />
+                    <img src={`/products/${product.sku}.jpg`} alt={product.title} />
                     <p>
                         {product.title}
                     </p>
