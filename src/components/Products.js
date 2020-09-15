ddeebfb10c278daad8dc78ab8cdb91a17d8966f5
MarkAnd-lab <mark-andersson@hotmail.com>
2020-09-15 09:22:24 +0200

    util.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import util from "../util";
 
 export default class Products extends Component {
     render() {
@@ -6,7 +7,7 @@ export default class Products extends Component {
             <div className="col-md-4">
             <div className="thumbnail text-center">
             <a href={`#${product.id}`} onClick={this.props.handleAddToCart}>
-                    <img src={`/products/${product.sku}.jpg`} alt={product.title} />
+                    <img src={`/products/${product.sku}_1.jpg`} alt={product.title} />
                     <p>
                         {product.title}
                     </p>
