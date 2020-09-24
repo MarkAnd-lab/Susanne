5cd08b7107b28da5a3391c1f88384b87c79400e0
MarkAnd-lab <mark-andersson@hotmail.com>
2020-09-24 11:50:12 +0200

    Filter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import "./App.css";
 import Products from './components/Products';
+import Filter from './components/Filter';
 class App extends Component {
   constructor(props){
     super(props);
@@ -22,10 +23,13 @@ class App extends Component {
 <hr/>
 <div className="row">
   <div className="col-md-8">
-
+<Filter size={this.state.size} sort={this.state.sort} handleChangeSize={this.handleChangeSize}
+handleChangeSort={this.handleChangeSort} count={this.state.filteredProducts.length} />
+<hr/>
+<Products products={this.state.filteredProducts} handledAddToCart={this.handledAddToCart}/>
   </div>
   <div className="col-md-4">
-<Products products={this.state.filteredProducts} handledAddToCart={this.handledAddToCart}/>
+
   </div>
 </div>
       
